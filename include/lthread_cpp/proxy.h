14340d4b4f94fc7d2cc55b01a805907356fa73d3
Hasan Alayli <hasan@webmon.com>
2014-10-15 17:44:47 +0000

    add client/server onData callbacks

--- include/lthread_cpp/proxy.h
+++ include/lthread_cpp/proxy.h
@@ -3,6 +3,9 @@
 
 #include "lthread_cpp/socket.h"
 
+#include <functional>
+#include <vector>
+
 namespace lthread_cpp {
 namespace net {
 
@@ -19,16 +22,33 @@ class SocketProxy {
   // starting point after a tcp connection is accepted
   void Run(const bool* shutdown);
 
+  using OnDataCallback = std::function<void(const char*, size_t)>;
+
+  void OnClientSend(OnDataCallback);
+  void OnClientRecv(OnDataCallback);
+
+  void OnServerSend(OnDataCallback);
+  void OnServerRecv(OnDataCallback);
+
  private:
+
   void RecvFromClient();
   void RecvFromServer();
-  void SendRecv(Socket* from, Socket* to);
+  void SendRecv(Socket* from,
+                Socket* to,
+                const std::vector<OnDataCallback>& recv_callbacks,
+                const std::vector<OnDataCallback>& send_callbacks);
 
   Lthread server_thread_;
   Lthread client_thread_;
 
   Socket* client_;
   Socket* server_;
+
+  std::vector<OnDataCallback> on_client_recv_;
+  std::vector<OnDataCallback> on_client_send_;
+  std::vector<OnDataCallback> on_server_recv_;
+  std::vector<OnDataCallback> on_server_send_;
 
   bool keep_running_;
   const bool* shutdown_;
--- src/proxy.cc
+++ src/proxy.cc
@@ -21,22 +21,31 @@ void SocketProxy::Run(const bool* shutdown)
 
 void SocketProxy::RecvFromClient()
 {
-  SendRecv(client_, server_);
+  SendRecv(client_, server_, on_client_recv_, on_server_send_);
 }
 
 void SocketProxy::RecvFromServer()
 {
-  SendRecv(server_, client_);
+  SendRecv(server_, client_, on_server_recv_, on_client_send_);
 }
 
-void SocketProxy::SendRecv(Socket* client, Socket* server)
+void SocketProxy::SendRecv(Socket* client,
+                           Socket* server,
+                           const std::vector<OnDataCallback>& recv_callbacks,
+                           const std::vector<OnDataCallback>& send_callbacks)
 {
   while(!shutdown_ && keep_running_)
   {
     char buf[1024];
     try {
       size_t sz = client->Recv(buf, 1024, 5000);
+      // run callbacks interested in what was recved
+      for (auto f : recv_callbacks)
+        f(buf, sz);
       server->Send(buf, sz);
+      // run callbacks interested in what was sent
+      for (auto f : send_callbacks)
+        f(buf, sz);
     } catch (SocketTimeout& e) {
       continue;
     } catch (SocketException& e) {
@@ -44,4 +53,24 @@ void SocketProxy::SendRecv(Socket* client, Socket* server)
     }
   }
   keep_running_ = false;
+}
+
+void SocketProxy::OnClientSend(OnDataCallback callback)
+{
+  on_client_send_.push_back(callback);
+}
+
+void SocketProxy::OnClientRecv(OnDataCallback callback)
+{
+  on_client_recv_.push_back(callback);
+}
+
+void SocketProxy::OnServerSend(OnDataCallback callback)
+{
+  on_server_send_.push_back(callback);
+}
+
+void SocketProxy::OnServerRecv(OnDataCallback callback)
+{
+  on_server_recv_.push_back(callback);
 }
